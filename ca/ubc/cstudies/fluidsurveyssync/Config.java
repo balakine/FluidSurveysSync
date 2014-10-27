9e1fe217f6a423ce1114bf01e55fd54394fc08f8
sgm2005@gmail.com <Sgm2005@>
2014-10-27 00:41:06 -0700

    4

--- ca/ubc/cstudies/fluidsurveyssync/Config.java
+++ ca/ubc/cstudies/fluidsurveyssync/Config.java
@@ -1,7 +1,36 @@
 package ca.ubc.cstudies.fluidsurveyssync;
 
-/**
- * Created by balakine on 10/26/2014.
- */
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Config {
+    public static String FLUID_SURVEYS_URL;
+    public static String MYSQL_URL;
+    public static String SRS_URL;
+    static {
+        // Load configuration
+        Properties config = new Properties();
+        try {
+            FileReader in = new FileReader("FluidSurveys.properties");
+            config.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FLUID_SURVEYS_URL = "https://" +
+                config.getProperty("FS.ApiKey") + ":" +
+                config.getProperty("FS.Password") + "@" +
+                config.getProperty("FS.ServerName");
+        MYSQL_URL = "jdbc:mysql://" +
+                config.getProperty("MySQL.Host") + "/" +
+                config.getProperty("MySQL.Database") + "?user=" +
+                config.getProperty("MySQL.User") + "&password=" +
+                config.getProperty("MySQL.Password");
+        SRS_URL = "jdbc:oracle:thin:" +
+                config.getProperty("SRS.User") + "/" +
+                config.getProperty("SRS.Password") + "@(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(PORT=1521)(HOST=" +
+                config.getProperty("SRS.Host") + "))(CONNECT_DATA=(SERVICE_NAME=" +
+                config.getProperty("SRS.ServiceName") + ")))";
+    }
 }
